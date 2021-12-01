70fcc65d96f0b390155bd20fad573edbdac2b8e6
xianzhi-bin <834411246@qq.com>
2021-12-02 00:57:06 +0800

    项目初始结构创建

---
+++ gulimai-member/src/test/java/com/atguigu/xianzhimail/member/GulimaiMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.xianzhimail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimaiMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
